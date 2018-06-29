6542c8c0e2122d73361aa923c8cfadebf70166c3
Abhishek Nagekar <abhishek@nagekar.com>
2018-06-30 01:05:49 +0530

    refactoring app.py

--- app.py
+++ app.py
@@ -7,26 +7,31 @@ import os
 
 app = Flask(__name__)
 
-# index is going to be needing the following data
-# current_weather { temprature
-# network_speed { upload, download }
-# system_usage { cpu, ram }
+# signup and get api key from here: https://openweathermap.org/appid
+# the api also supports city param
+LAT = '19.45'
+LON = '73.33'
 WEATHER_API_KEY = os.environ['OPENWEATHERAPI']
-WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather?lat=19.45&lon=73.33&appid=' + WEATHER_API_KEY
+WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'.format(LAT, LON, WEATHER_API_KEY)
 
-TX = None
-RX = None
+# set the interface name to monitor
+INTERFACE_NAME = 'wlan0'
+TX_FILE = '/sys/class/net/{}/statistics/tx_bytes'.format(INTERFACE_NAME)
+RX_FILE = '/sys/class/net/{}/statistics/rx_bytes'.format(INTERFACE_NAME)
+
 
 def get_current_weather():
-    """API response
-    {'sys': {'sunrise': 1530145873, 'id': 7761, 'type': 1, 'sunset': 1530193733, 'message': 0.0057, 'country': 'IN'}, 'wind': {'speed': 2.1, 'deg': 160}, 'name': 'Shahapur', 'main': {'temp': 300.15, 'temp_min': 300.15, 'humidity': 94, 'pressure': 1002, 'temp_max': 300.15}, 'clouds': {'all': 75}, 'visibility': 2500, 'cod': 200, 'dt': 1530212400, 'base': 'stations', 'coord': {'lat': 19.45, 'lon': 73.33}, 'id': 1256747, 'weather': [{'description': 'mist', 'id': 701, 'icon': '50n', 'main': 'Mist'}]}
+    """
+        Calls the api and returns temperature and city name.
+        Interesting data checks out
+        TODO: more data can be used to create something visual and interesting here.
     """
     data_raw = urllib.request.urlopen(WEATHER_API).read()
     data = json.loads(data_raw.decode('ascii'))
 
+    # convert from kelvin to celsius
     temperature = int(data['main']['temp']) - 273
     name = data['name']
-    # icon = data['weather'][0]['icon'] + 'n' + '.png'
 
     return {
             'temperature': temperature,
@@ -35,36 +40,52 @@ def get_current_weather():
 
 
 def get_txrx():
-    with open('/sys/class/net/wlan0/statistics/rx_bytes') as f:
+    """
+    opens the network stats file for the said interface, record the packet count for each tx and rx
+    wait for 250ms and then record again
+    difference multiplied by 4 is the bytes transferred per second
+    :return:
+    """
+
+    with open(RX_FILE) as f:
         RX = int(f.read().rstrip())
 
-    with open('/sys/class/net/wlan0/statistics/tx_bytes') as f:
+    with open(TX_FILE) as f:
         TX = int(f.read().rstrip())
 
     time.sleep(0.25)
 
-    with open('/sys/class/net/wlan0/statistics/rx_bytes') as f:
+    with open(RX_FILE) as f:
         RX = int(f.read().rstrip()) - RX
 
-    with open('/sys/class/net/wlan0/statistics/tx_bytes') as f:
+    with open(TX_FILE) as f:
         TX = int(f.read().rstrip()) - TX
 
-    return {
-        'TX': TX,
-        'RX': RX
-    }
-
-def get_network_stats():
-    data = get_txrx()
-    
     # multiply output by 4 because the interval is 1/4th of a second
     return {
-        'upload': data['TX'] * 4,
-        'download': data['RX'] * 4
+        'TX': TX * 4,
+        'RX': RX * 4
+    }
+
+
+def get_network_stats():
+    """
+    get data from helper function and return a formatted dictionary
+    :return: { upload: xx, download: yy }
+    """
+    data = get_txrx()
+    
+    return {
+        'upload': data['TX'],
+        'download': data['RX']
     }
 
 
 def get_system_stats():
+    """
+    get cpu and ram usage percentages
+    :return: { cpu: xx, ram: yy }
+    """
     cpu_percent = psutil.cpu_percent()
     ram = psutil.virtual_memory().percent
 
@@ -78,13 +99,20 @@ def get_system_stats():
 def index():
     return render_template('index.html')
 
+
 @app.route('/data')
 def data_endpoint():
+    """
+    this is our 'rest' api, the data cow
+    this api is polled continously,
+    :return: weather, network and system stats
+    """
     current_weather = get_current_weather()
     network = get_network_stats()
     system = get_system_stats()
 
     return jsonify({'weather': current_weather, 'network': network, 'system': system})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
